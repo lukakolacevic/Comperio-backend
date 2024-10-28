179322c0dd173a2940c6147ae291ba8f549c9d9a
lukakolacevic <lukakolacevic@gmail.com>
2024-10-28 18:12:01 +0100

    Create RemoveProfessorFromSubjectModel.cs

---
+++ ViewModels/RemoveProfessorFromSubjectModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotInstrukcijeBackend.ViewModels
+{
+    public class RemoveProfessorFromSubjectModel
+    {
+        public int ProfessorId { get; set; }
+
+        public int SubjectId { get; set; }
+    }
+}
