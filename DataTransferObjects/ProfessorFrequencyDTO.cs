af86f19cc7936147bb1d7de6863490b9d89e1d95
lukakolacevic <lukakolacevic@gmail.com>
2024-11-02 19:57:49 +0100

    Create ProfessorFrequencyDTO.cs

---
+++ DataTransferObjects/ProfessorFrequencyDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace dotInstrukcijeBackend.DataTransferObjects
+{
+    public class ProfessorFrequencyDTO
+    {
+        public string ProfessorName { get; set; }
+        public string ProfessorSurname { get; set; }
+        public int SessionCount { get; set; }
+    }
+}
