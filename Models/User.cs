09418a8538b73bcc8b03c9b437482f8e74f4ed5d
lukakolacevic <lukakolacevic@gmail.com>
2024-12-22 22:53:16 +0100

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using dotInstrukcijeBackend.ProfilePictureSavingUtility;
+using System.Text.Json.Serialization;
+
+namespace dotInstrukcijeBackend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        
+        [JsonIgnore]
+        public string PasswordHash { get; set; }
+
+        [JsonConverter(typeof(ByteArrayToBase64Converter))]
+        public byte[]? ProfilePicture { get; set; }
+        [JsonIgnore]
+        public string? OAuthId { get; set; }
+        [JsonIgnore]
+        public DateTime CreatedAt { get; set; }
+        [JsonIgnore]
+        public bool IsVerified { get; set; }
+    }
+}
